51b8894b08212ea8ec6eff5cfaaf6deadca98ce3
sltcestloic <root@sltcestloic.dev>
2024-06-07 09:56:28 +0200

    check that json file exist, parse with yojson

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,6 @@
+open Yojson.Basic.Util
+open Printf
+
 let () =
   if Array.length Sys.argv = 1 then
     Printf.printf "Invalid arguments, try --help\n"
@@ -11,8 +14,23 @@ let () =
   )
   else if Array.length Sys.argv = 2 then
     Printf.printf "Invalid arguments, try --help\n"
-  else
-  for i = 0 to Array.length Sys.argv - 1 do
-    Printf.printf "[%i] %s\n" i Sys.argv.(i)
-  done
+  else (
+    for i = 0 to Array.length Sys.argv - 1 do
+      Printf.printf "[%i] %s\n" i Sys.argv.(i);
+    done;
 
+    let filename = Sys.argv.(1) in
+    let input = Sys.argv.(2) in
+    if not (Sys.file_exists filename) then (
+      printf "File %s does not exist.\n" filename;
+      exit 1
+    );
+
+    try 
+      let json = Yojson.Basic.from_file filename in
+      printf "File %s is a valid JSON file.\n" filename;
+    with
+    | Yojson.Json_error msg ->
+      printf "File %s is not a valid JSON file: %s\n" filename msg;
+      exit 1
+  )
